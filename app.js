80a1c7acf89fb0c89664ec3e69e5d77a75a2f8c2
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-04-10 21:45:57 +0200

    format documents

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,12 +28,12 @@ app.use('/users', usersRouter);
 app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
